f91977983108852b82dc31195fcf4408b954197c
Kushagar Bansal <101501748+kb200781@users.noreply.github.com>
2022-11-22 15:54:39 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -90,7 +90,7 @@ if go:
   explode = (0.1,0.1,0.1)
   tags.plot(kind='pie', autopct='%1.1f%%', shadow=True, colors = colors,
             startangle=90, wedgeprops = wp, explode = explode, label='')
-  plt.title('Distribution fo sentiments')
+  plt.title('Distribution of sentiments')
   plt.show()
   st.set_option('deprecation.showPyplotGlobalUse', False)
   st.pyplot()
